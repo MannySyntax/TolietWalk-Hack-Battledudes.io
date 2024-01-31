57c337079534efc845e84c311f16ab336c53575c
Manny <109997233+MannySyntax@users.noreply.github.com>
2024-01-31 13:55:11 -0500

    Create Module.js

---
+++ Module.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         TolietWalk - Battledudes.io
+// @namespace    http://tampermonkey.net/
+// @description  The best publicized battledudes.io cheat | By MannyCodes
+// @author       MannyCodes
+// @match        https://battledudes.io/*
+// @grant        none
+// @icon         https://i.ibb.co/D1mVRx3/images.jpg
+// @require      https://code.jquery.com/jquery-3.7.0.min.js
+// @require      https://gist.githubusercontent.com/MannySyntax/05e1eaedf7dffa104df10f46b7feb28f/raw/2dc1fdf9359d1f11540e539fd92fd58d128c0af6/TolietWalk-Client-Menu
+// @require      https://gist.githubusercontent.com/MannySyntax/d8e78a39356cfc1276c0487285931e3b/raw/1053c30e096abb5d07a5805ef922ed676a3016cf/TolietWalk.js
+// ==/UserScript==
+
+/* `
+___________    .__  .__        __     __      __        .__   __    
+\__    ___/___ |  | |__| _____/  |_  /  \    /  \_____  |  | |  | __
+  |    | /  _ \|  | |  |/ __ \   __\ \   \/\/   /\__  \ |  | |  |/ /
+  |    |(  <_> )  |_|  \  ___/|  |    \        /  / __ \|  |_|    < 
+  |____| \____/|____/__|\___  >__|     \__/\  /  (____  /____/__|_ \
+                            \/              \/        \/          \/
+
+  Author: MannyCodes
+  Features
+   - Aimbot (Z Key)
+   - ESP/Tracers (X Key)
+   - Zoom (C Key)
+*/
+
