eed008f50df21e4a90b7c0a7135d75f85c8f903e
Manny <109997233+MannySyntax@users.noreply.github.com>
2024-02-02 16:47:08 -0500

    Update Module.js

--- Module.js
+++ Module.js
@@ -8,7 +8,7 @@
 // @icon         https://i.ibb.co/D1mVRx3/images.jpg
 // @require      https://code.jquery.com/jquery-3.7.0.min.js
 // @require      https://gist.githubusercontent.com/MannySyntax/05e1eaedf7dffa104df10f46b7feb28f/raw/2dc1fdf9359d1f11540e539fd92fd58d128c0af6/TolietWalk-Client-Menu
-// @require      https://gist.githubusercontent.com/MannySyntax/d8e78a39356cfc1276c0487285931e3b/raw/1053c30e096abb5d07a5805ef922ed676a3016cf/TolietWalk.js
+// @require      https://gist.githubusercontent.com/MannySyntax/d8e78a39356cfc1276c0487285931e3b/raw/41fd5613acf11700a24376f332601b7f7abdf873/TolietWalk.js
 // ==/UserScript==
 
 /* `
